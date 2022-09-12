4c0af9eb5df5692a7ceb1b70fae8335996228c8f
BootCampVM1\BootCamp <bootcamp@bootcamp.com>
2022-09-12 16:15:06 +0200

    Recursion_factorial

--- src/Lesson47/Test.java
+++ src/Lesson47/Test.java
@@ -2,27 +2,15 @@ package Lesson47;
 
 public class Test {
     public static void main(String[] args) {
+        Season season = Season.AUTUMN;
         Animal animal = Animal.CAT;
-        System.out.println(animal);
+        System.out.println(animal.name());
+        System.out.println(season.name());
 
+        Animal frog = Animal.valueOf("FROG");
+        System.out.println(frog.getTranslation());
 
-        switch(animal) {
-            case CAT:
-                System.out.println("It's a cat");
-                break;
-            case DOG:
-                System.out.println("It's a dog");
-        }
-        Season season = Season.SUMMER;
-        System.out.println(season.getTemperature());
-
-        switch (season) {
-            case SUMMER:
-                System.out.println("It's warm outside");
-                break;
-            case WINTER:
-                System.out.println("It's cold outside");
-                break;
-        }
+        Season winter = Season.WINTER;
+        System.out.println(winter.ordinal());
     }
 }
